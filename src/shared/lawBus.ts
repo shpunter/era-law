af571d29f5c7ba63a1a8b590a3b64c4793680d24
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2026-06-17 23:48:46 +0200

    add missing factions

--- src/shared/lawBus.ts
+++ src/shared/lawBus.ts
@@ -12,7 +12,7 @@ import { BehaviorSubject, Subject } from "rxjs";
 
 const initialState: LawState = {
   down: {
-    faction: "grove",
+    faction: "temple",
     historyIDX: 0,
     resLaw: 143_000,
   },
